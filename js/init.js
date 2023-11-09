6c6aa4a949f71e66910b1921f30c0178cf6503c4
aut43iamc <150367975+aut43iamc@users.noreply.github.com>
2023-11-09 10:16:13 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,10 +2,10 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyBNo_3zDq_4Fyy6RHRA842-235J9_7hXDY",
-  authDomain: "autenticacionwithfb.firebaseapp.com",
-  projectId: "autenticacionwithfb",
-  storageBucket: "autenticacionwithfb.appspot.com",
-  messagingSenderId: "563607923061",
-  appId: "1:563607923061:web:0f26e6dd6d48120687cb3c"
+ apiKey: "AIzaSyBWPhG9xbFxithmy_cATRUyJavEcB5nxY4",
+    authDomain: "kljl-bf40d.firebaseapp.com",
+    projectId: "kljl-bf40d",
+    storageBucket: "kljl-bf40d.appspot.com",
+    messagingSenderId: "869196094621",
+    appId: "1:869196094621:web:20c98286214e7dfe560874"
     });
